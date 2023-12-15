dd7ed7495947a60c036d7eb9d19cc9fb885e2bda
Aniko Sallai <aniko.sallai@gmail.com>
2023-12-15 21:34:10 +0100

    107. Browser Setup for Scraping

--- scraper.ts
+++ scraper.ts
@@ -1,1 +1,20 @@
-console.log("Hello World")
+const playwright = require('playwright')
+
+const BASE_URL = "https://github.com/topics/playwright"
+
+; (async () => {
+    // Setup browser
+    const browser = await playwright.chromium.launch({ headless: true})
+    const context = await browser.newContext()
+    const page = await context.newPage({ bypassCSP: true })
+    await page.setDefaultTimeout(30000)
+    await page.setViewportSize({ width: 800, height: 600})
+    await page.goto(BASE_URL)
+    
+    // Close browser
+    await browser.close()
+
+}) ().catch(error => {
+    console.log(error)
+    process.exit(1)
+})
