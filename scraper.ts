2ef814fdab9df6b482b7d33ea2c0583151cf8ba4
Aniko Sallai <aniko.sallai@gmail.com>
2023-12-16 21:18:06 +0100

    108. Set User Agent

--- scraper.ts
+++ scraper.ts
@@ -1,15 +1,23 @@
-const playwright = require('playwright')
+const playwright = require("playwright")
+const random_useragent = require("random-useragent")
 
 const BASE_URL = "https://github.com/topics/playwright"
 
 ; (async () => {
+    // Create random user agent
+    const agent = random_useragent.getRandom()
+
     // Setup browser
     const browser = await playwright.chromium.launch({ headless: true})
-    const context = await browser.newContext()
+    const context = await browser.newContext({ userAgent: agent})
     const page = await context.newPage({ bypassCSP: true })
     await page.setDefaultTimeout(30000)
     await page.setViewportSize({ width: 800, height: 600})
     await page.goto(BASE_URL)
+
+    // Get data from website
+
+    // Store data into file
     
     // Close browser
     await browser.close()
